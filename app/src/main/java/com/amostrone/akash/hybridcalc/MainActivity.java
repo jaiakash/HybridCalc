403241b4d5aeafa3b79ac7914574b3e8640a1ba3
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-24 17:20:52 +0530

    Added Input fragment.

--- app/src/main/java/com/amostrone/akash/hybridcalc/MainActivity.java
+++ app/src/main/java/com/amostrone/akash/hybridcalc/MainActivity.java
@@ -6,9 +6,18 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public MainActivity() {
+        super(R.layout.activity_main);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, inputFragment.class, null)
+                    .commit();
+        }
     }
 }

323c1fafe91cf9b912869759dc2d4750cc5a1c9c
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-24 21:01:23 +0530

    Basic calculator done. 2/2

--- app/src/main/java/com/amostrone/akash/hybridcalc/MainActivity.java
+++ app/src/main/java/com/amostrone/akash/hybridcalc/MainActivity.java
@@ -11,6 +11,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    float a_val=0,b_val=0,ans=0;
+    boolean operation_pressed=false;
+    int op=0;
+    String operation="None";
+
     public MainActivity() {
         super(R.layout.activity_main);
     }
@@ -30,43 +35,99 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void calc_clicked(View view) {
-        EditText edtone = findViewById(R.id.editText1);
-        EditText edttwo = findViewById(R.id.editText2);
-        float a,b,ans;
-        if(TextUtils.isEmpty(edtone.getText()))
-        {
-            edtone.setError(" Please enter the number");
-            return;
-        }else {
-            a = Float.parseFloat(edtone.getText().toString());
-        }
-        if(TextUtils.isEmpty(edttwo.getText()))
-        {
-            edttwo.setError(" Please enter the number ");
-            return;
-        }else {
-            b = Float.parseFloat(edttwo.getText().toString());
-        }
-        ans=0;
+    float getnum(View view,float a){
         switch (view.getId()) {
-            case R.id.plus:
-                ans=a+b;
+            case R.id.one:
+                a=a*10+1;
                 break;
-            case R.id.minus:
-                ans=a-b;
+            case R.id.two:
+                a=a*10+2;
+                break;
+            case R.id.three:
+                a=a*10+3;
+                break;
+            case R.id.four:
+                a=a*10+4;
+                break;
+            case R.id.five:
+                a=a*10+5;
+                break;
+            case R.id.six:
+                a=a*10+6;
+                break;
+            case R.id.seven:
+                a=a*10+7;
+                break;
+            case R.id.eight:
+                a=a*10+8;
+                break;
+            case R.id.nine:
+                a=a*10+9;
+                break;
+            case R.id.zero:
+                a=a*10+0;
+                break;
+        }
+        return a;
+    }
+
+    public void calc_clicked(View view) {
+
+        switch (view.getId()) {
+            case R.id.add:
+                operation_pressed=true;
+                operation="Addition";
+                op=1;
+                break;
+            case R.id.sub:
+                operation_pressed=true;
+                operation="Subtraction";
+                op=2;
                 break;
             case R.id.mul:
-                ans=a*b;
+                operation_pressed=true;
+                operation="Multiplication";
+                op=3;
                 break;
             case R.id.div:
-                if(b!=0) ans=a/b;
-                else ans=-1;
+                operation_pressed=true;
+                operation="Division";
+                op=4;
                 break;
+            case R.id.ans:
+                switch(op){
+                    case 1:
+                        ans=a_val+b_val;
+                        break;
+                    case 2:
+                        ans=a_val-b_val;
+                        break;
+                    case 3:
+                        ans=a_val*b_val;
+                        break;
+                    case 4:
+                        if(b_val!=0) ans=a_val/b_val;
+                        else ans=-1;
+                        break;
+                }
+                break;
+            case R.id.decimal:
+                //TODO decimal
+                ans=ans;
+                break;
+            default:
+                if(a_val==0 || !operation_pressed) a_val = getnum(view,a_val);
+                else if(b_val==0 || operation_pressed) b_val = getnum(view,b_val);
         }
 
-        Toast.makeText(this, ans+"", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, a+"", Toast.LENGTH_SHORT).show();
         TextView edtans = findViewById(R.id.ansTextView);
+        TextView opTV = findViewById(R.id.opTextView);
+        TextView TVa = findViewById(R.id.aTextView);
+        TextView TVb = findViewById(R.id.bTextView);
+        opTV.setText("Operation = "+operation);
+        TVa.setText("A = "+a_val);
+        TVb.setText("B = "+b_val);
         edtans.setText("Your ans is = "+ans);
     }
 }

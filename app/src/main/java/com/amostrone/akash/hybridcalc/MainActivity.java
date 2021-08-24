c0c4dd5365bd619d4e3fbbdc64da77c429a07753
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-24 17:46:03 +0530

    Added calculation.

--- app/src/main/java/com/amostrone/akash/hybridcalc/MainActivity.java
+++ app/src/main/java/com/amostrone/akash/hybridcalc/MainActivity.java
@@ -3,6 +3,9 @@ package com.amostrone.akash.hybridcalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +22,29 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.fragment_container_view, inputFragment.class, null)
                     .commit();
         }
+    }
+
+    public void calc_clicked(View view) {
+        EditText edtone = findViewById(R.id.editText1);
+        EditText edttwo = findViewById(R.id.editText2);
+        float a = Float.parseFloat(edtone.getText().toString());
+        float b = Float.parseFloat(edttwo.getText().toString());
+        float ans=0;
+        switch (view.getId()) {
+            case R.id.plus:
+                ans=a+b;
+                break;
+            case R.id.minus:
+                ans=a-b;
+                break;
+            case R.id.mul:
+                ans=a*b;
+                break;
+            case R.id.div:
+                if(b!=0) ans=a/b;
+                else ans=-1;
+                break;
+        }
+        Toast.makeText(this, ans+"", Toast.LENGTH_SHORT).show();
     }
 }
